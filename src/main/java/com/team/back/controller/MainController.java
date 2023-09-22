941623e66f4aee026c99fe94c37d23b76e1e59ad
jhs1145 <jhs1145@gmail.com>
2023-09-22 14:57:50 +0900

    add hello method

--- src/main/java/com/team/back/controller/MainController.java
+++ src/main/java/com/team/back/controller/MainController.java
@@ -1,8 +1,14 @@
 package com.team.back.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
   
+     @GetMapping("")
+     public String hello() {
+          return "Hello";
+     }
+
 }
