6e82fa316b388e749db6f9587181cc515b6e3f62
Sooin <sooin0458@gmail.com>
2023-10-03 21:59:19 +0900

    sooin merge

--- src/main/java/com/team/back/config/WebSecurityConfig.java
+++ src/main/java/com/team/back/config/WebSecurityConfig.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
